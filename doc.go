3f105e1bd475b5c6d5e6e013e66787b34249374f
Riftbit ErgoZ <ergozru@gmail.com>
2018-08-08 21:36:55 +0300

    fix doc.go file

--- doc.go
+++ doc.go
@@ -1,6 +1,14 @@
-package jrpc2_client
+// Copyright 2018 Riftbit ErgoZ. All rights reserved.
+// license that can be found in the LICENSE file.
 
-// This is a json-rpc 2.0 client package for golang based on:
-// - HTTP Client: github.com/erikdubbelboer/fasthttp
-// - JSON Parser: github.com/pquerna/ffjson/ffjson
-// - Logger: github.com/Sirupsen/logrus
+/*
+	Package jrpc2_client implements client for json-rpc 2.0 protocol
+	and based on another packages:
+	 - HTTP Client: github.com/erikdubbelboer/fasthttp
+	 - JSON Parser: github.com/pquerna/ffjson/ffjson
+	 - Logger: github.com/Sirupsen/logrus
+
+	Example can be found only in client_test.go at this moment
+*/
+
+package jrpc2_client
